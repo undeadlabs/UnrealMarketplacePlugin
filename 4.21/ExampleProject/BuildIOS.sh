3e62c6b966f7f0bc353a7a8d34e54c72520153f6
Michael Mackend <michael.mackend@microsoft.com>
2019-05-29 10:28:27 -0700

    change the cli params to match what the editor does when building ios

--- 4.21/ExampleProject/BuildIOS.sh
+++ 4.21/ExampleProject/BuildIOS.sh
@@ -8,20 +8,19 @@ uePath="/Users/Shared/Epic Games"
 ueVersion="4.21"
 
 # Full Path. In theory it should not be edited (the engine path/version are the only ones that should be edited).
-uatPath="$uePath"/UE_"$ueVersion"/Engine/Build/BatchFiles/
+uatPath="$uePath/UE_${ueVersion}/Engine/Build/BatchFiles/"
 
 # Target Platform
 targetPlatform=iOS
 
-currentdir=`pwd`
-
-uprojdir="$currentdir"/ExampleProject.uproject
-
-currentdir=`pwd`
+uprojdir="$PWD/ExampleProject.uproject"
 
 # Destination Path, where the build will end up (relative to active directory).
 archivePath=Build/"$targetPlatform"
+
 pushd "$uatPath"
 # The actual build command. To adapt depending on the needs.
-. RunUAT.sh BuildCookRun -clean -rocket -installed -nop4 -nocompile -project="$uprojdir" -cook -stage -archive -archivedirectory="$archivePath" -package -clientconfig=Development -Platform="$targetPlatform" -compressed -CookAll -pak -prereqs -build -utf8output -editorrecompile
+#. RunUAT.sh BuildCookRun -clean -rocket -installed -nop4 -nocompile -project="$uprojdir" -cook -stage -archive -archivedirectory="$archivePath" -package -clientconfig=Development -Platform="$targetPlatform" -compressed -CookAll -pak -prereqs -build -utf8output -editorrecompile
+./RunUAT.sh -ScriptsForProject="$uprojdir" BuildCookRun -nocompile -nocompileeditor -installed -nop4 -project="$uprojdir" -cook -stage -archive -archivedirectory="$PWD/Build" -package -clientconfig=Development -ue4exe=UE4Editor -pak -prereqs -nodebuginfo -targetplatform=IOS -build -utf8output
+
 popd
